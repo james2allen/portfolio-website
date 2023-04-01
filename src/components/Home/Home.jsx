1e760b90ad0af3844fa0edd9ded40cdc57d669fa
James <james2allen@gmail.com>
2023-04-01 17:55:58 -0400

    Added resume & small changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AnimatedText from "react-animated-text-content";
 
 export const Home = () => {
   return (
@@ -6,9 +7,26 @@ export const Home = () => {
       <div className="flex w-8/12 justify-center">
         <div className="flex flex-col text-stone-50">
           <h1 className="text-8xl font-bold"> Welcome</h1>
-          <h1 className="pt-6 text-4xl font-bold">
+
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: "200px",
+              y: "-20px",
+              scale: 1.1,
+              ease: "ease-in-out",
+            }}
+            animationType="float"
+            interval={0.06}
+            duration={0.8}
+            tag="p"
+            className="animated-paragraph pt-6 text-4xl font-bold"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
+          >
             I'm James, a Web Developer
-          </h1>
+          </AnimatedText>
         </div>
       </div>
     </div>
---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+const resume_link =
+  "https://raw.githubusercontent.com/james2allen/portfolio/master/src/assets/James_Allen_Resume-website.pdf";
+
+export const Resume = () => {};
