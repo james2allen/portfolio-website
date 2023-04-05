09fc9d2add0da980f5d082caa7f3550efa3d2ff1
James <james2allen@gmail.com>
2023-04-04 20:48:32 -0400

    Animate background-position transition when nav items are hovered

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./SideNav.module.scss";
+import { clsx } from "clsx";
+import { useEffect } from "react";
 
 export const SideNav = () => {
+  const [activeItem, setActiveItem] = useState(-1);
+
+  useEffect(() => {
+    console.log(activeItem);
+  }, [activeItem]);
+
   return (
     <nav className={styles.nav}>
-      <div className={styles.navItems}>
-        <Link className={styles.navItem} to={"/"}>
+      <div
+        className={clsx(
+          styles.navItems,
+          activeItem === 0 && styles.activeItem0,
+          activeItem === 1 && styles.activeItem1,
+          activeItem === 2 && styles.activeItem2,
+          activeItem === 3 && styles.activeItem3
+        )}
+      >
+        <Link
+          className={styles.navItem}
+          to={"/"}
+          onMouseOver={() => setActiveItem(0)}
+        >
           Home
         </Link>
-        <Link className={styles.navItem} to="/projects">
+        <Link
+          className={styles.navItem}
+          to="/projects"
+          onMouseOver={() => setActiveItem(1)}
+        >
           Projects
         </Link>
-        <Link className={styles.navItem} to="/blog">
+        <Link
+          className={styles.navItem}
+          to="/blog"
+          onMouseOver={() => setActiveItem(2)}
+        >
           Blog
         </Link>
-        <Link className={styles.navItem} to="/resume">
+        <Link
+          className={styles.navItem}
+          to="/resume"
+          onMouseOver={() => setActiveItem(3)}
+        >
           Resume
         </Link>
       </div>
